cac779a1605385c79bb8f6efcd2ca1fbc83124ca
Ansoo <asc2683@gmail.com>
2017-06-05 17:50:56 -0400

    updated .gitignore

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base'
-
-const base = Rebase.createClass({
-  apiKey: 'apiKey',
-  authDomain: 'exam-school-v1.firebaseapp.com',
-  databaseURL: 'https://exam-school-v1.firebaseio.com'
-})
-
-export default base
