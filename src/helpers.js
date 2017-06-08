0179c5c3c347cd72145f097b08b46f15ad6b6985
Ansoo <asc2683@gmail.com>
2017-06-07 21:33:37 -0400

    fix(random key gen): fixed random key gen func

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,12 @@
 /* helper function */
 
 export function generateKey () {
-  return `p${Date.now()}-${Math.floor(Math.random() * 10000)}`
+  let randomText = ''
+  const alphaNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  for ( let i=0; i < 10; i++ ) {
+    randomText += alphaNum.charAt(Math.floor(Math.random() * alphaNum.length))
+  }
+  return `s${Date.now()}_${randomText}`
 }
+
